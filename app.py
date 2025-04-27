12105ed7f45099ced3f59c1fa94b75d4c5fed734
unknown <dpdeva30@gmail.com>
2025-04-27 19:24:55 +0530

    First commit

--- app.py
+++ app.py
@@ -99,5 +99,9 @@ def predict():
     
     return render_template('predict.html', result=result, feature_names=base_features)
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
